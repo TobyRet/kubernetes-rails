4ff383bae3bf3e87d312490b5f55348d39c39a8b
Toby Retallick <toby.retallick@gmail.com>
2018-09-06 13:13:11 +0100

    Create list scaffold
    - set root to list index
    - add seed for lists

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_09_06_120427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lists", force: :cascade do |t|
+    t.string "title"
+    t.text "excerpt"
+    t.text "description"
+    t.integer "upvotes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+List.create!(title: 'Top 10 movies', description: 'All the shiz I like watching', upvotes: 50)
+
+List.create!(title: 'Top 10 cakes', description: 'All the shiz I like to eat', upvotes: 30)
+
+puts 'All lists seeded'
